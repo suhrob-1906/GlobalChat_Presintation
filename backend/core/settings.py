7347391ff0921a72c2b8ca8f43eddded80dc2ab1
suhrob-1906 <suxrbjon10.m.s@gmail.com>
2025-12-18 15:50:14 +0500

    suhrob's commit

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -8,7 +8,7 @@ import os
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-secret")
 
-DEBUG = os.environ.get("DEBUG", "True") == "False"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
